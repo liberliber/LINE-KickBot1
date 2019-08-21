cca0ec0ca5cdb63e99a6f2eb4dd28eb09e7d6273
liberliber <53687918+liberliber@users.noreply.github.com>
2019-08-21 20:33:35 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,14 +9,14 @@ class LINE extends Command {
         this.stateStatus = {
             cancel: 1,
             kick: 1,
-            salam: 1
+            qrp: 1
         };
         this.messages;
         this.payload
     }
 
     get myBot() {
-        const bot = ['u1d55aeaa8b863cb338f4e8fd7a761b4b','u15ea9e62d7293bc69654e5e5f8a0053b','u277c188afb3f7632c261c58a79933360','u27dcf89bce7c8747107301e7b4424e08','u3a1641641abbf666b61a09221748d10c','u413abe1a4bbe3f646c8edab032fd6117','u43a4a9e76c47863e4a839b39b95c7047','u5ed385b3f578a1552439d75ada3eaea4','u605205b7305427eb778b499dbf4aebc8','u630ee831febbe7240973364570541171','u9882ac8a197b20f25cc21650f656fadd','ub1594fa0d3959eb82ac73675541d7ee0','ubc56b8fda25e78795ce157905a7fe6f2','ube3899df6730441eae28c3dc2f109249','uc687709232ddb16870499e0e9687c8ed','ue7afb5f5fd1b16902f22f29b9c9c9970','ufedfd6819d63f9235007cab688acebe7','u382280cbc27f30b9393b17d87ac4c569'];
+        const bot = ['u868f18020f65c1887fba07cbf44cb1d5','u15ea9e62d7293bc69654e5e5f8a0053b','u277c188afb3f7632c261c58a79933360','u27dcf89bce7c8747107301e7b4424e08','u3a1641641abbf666b61a09221748d10c','u413abe1a4bbe3f646c8edab032fd6117','u43a4a9e76c47863e4a839b39b95c7047','u5ed385b3f578a1552439d75ada3eaea4','u605205b7305427eb778b499dbf4aebc8','u630ee831febbe7240973364570541171','u9882ac8a197b20f25cc21650f656fadd','ub1594fa0d3959eb82ac73675541d7ee0','ubc56b8fda25e78795ce157905a7fe6f2','ube3899df6730441eae28c3dc2f109249','uc687709232ddb16870499e0e9687c8ed','ue7afb5f5fd1b16902f22f29b9c9c9970','ufedfd6819d63f9235007cab688acebe7','u382280cbc27f30b9393b17d87ac4c569'];
         return bot; 
     }
 
@@ -40,6 +40,23 @@ class LINE extends Command {
             this.receiverID = message.to = (operation.message.to === this.myBot[0]) ? operation.message._from : operation.message.to ;
             Object.assign(message,{ ct: operation.createdTime.toString() });
             this.textMessage(message)
+        }
+
+        if(operation.type == 17) {
+            this._kickMember(operation.param1,[operation.param2]);
+        }
+
+        if(operation.type == 11 && this.stateStatus.qrp == 1) {
+            this._kickMember(operation.param1,[operation.param2]);
+            this.messages.to = operation.param1;
+            this.qrOpenClose();
+        }
+
+        if(operation.type == 19) {
+            if(this.isAdminOrBot(operation.param3)) {
+                this._kickMember(operation.param1,[operation.param2]);
+                this._invite(operation.param1,[operation.param3]);
+            }
         }
 
         if(operation.type == 13) {
@@ -74,8 +91,8 @@ class LINE extends Command {
         let receiver = messages.to;
         let sender = messages.from;
         
-        this.command('speed', this.getSpeed.bind(this));
-        this.command(`huhu ${payload}`,this.cancelMember.bind(this));
+        this.command('sp3ed', this.getSpeed.bind(this));
+        this.command(`huftt ${payload}`,this.kickMember.bind(this));
     }
 
 }
